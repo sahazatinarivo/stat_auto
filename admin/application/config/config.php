0a93df1c9039a8e29177cc99a990d2d8b5233eca
sahazatinarivo <sahazatinarivo@gmail.com>
2022-03-25 09:59:09 +0300

    commit config

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/projet/stat_auto/admin/';
+$config['base_url'] = 'http://localhost/projet/stat_auto_online/stat_auto/admin/';
 
 /*
 |--------------------------------------------------------------------------
